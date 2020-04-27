5af7972e6679960f3a15575a1c5f88f0f3a86912
Notedop <raoul2405@hotmail.com>
2020-04-27 13:49:29 +0200

    initial test

--- cc1100_raspi.cpp
+++ cc1100_raspi.cpp
@@ -317,7 +317,55 @@ static uint8_t cc1100_OOK_4_8_kb[CFG_REGISTER] = {
                     0x35,  // TEST1         Various Test Settings
                     0x09,  // TEST0         Various Test Settings
                };
-
+static uint8_t cc1100_OOK_SA_33[CFG_REGISTER] = {
+        0x29,  // IOCFG2        GDO2 Output Pin Configuration
+        0x2E,  // IOCFG1        GDO1 Output Pin Configuration
+        0x06,  // IOCFG0        GDO0 Output Pin Configuration
+        0x47,  // FIFOTHR       RX FIFO and TX FIFO Thresholds
+        0xD3,  // SYNC1         Sync Word, High Byte
+        0x91,  // SYNC0         Sync Word, Low Byte
+        0xFF,  // PKTLEN        Packet Length
+        0x04,  // PKTCTRL1      Packet Automation Control
+        0x05,  // PKTCTRL0      Packet Automation Control
+        0x00,  // ADDR          Device Address
+        0x00,  // CHANNR        Channel Number
+        0x06,  // FSCTRL1       Frequency Synthesizer Control
+        0x00,  // FSCTRL0       Frequency Synthesizer Control
+        0x10,  // FREQ2         Frequency Control Word, High Byte
+        0xB0,  // FREQ1         Frequency Control Word, Middle Byte
+        0x3F,  // FREQ0         Frequency Control Word, Low Byte
+        0xF5,  // MDMCFG4       Modem Configuration
+        0x83,  // MDMCFG3       Modem Configuration
+        0x33,  // MDMCFG2       Modem Configuration
+        0x00,  // MDMCFG1       Modem Configuration
+        0xF8,  // MDMCFG0       Modem Configuration
+        0x15,  // DEVIATN       Modem Deviation Setting
+        0x07,  // MCSM2         Main Radio Control State Machine Configuration
+        0x30,  // MCSM1         Main Radio Control State Machine Configuration
+        0x18,  // MCSM0         Main Radio Control State Machine Configuration
+        0x16,  // FOCCFG        Frequency Offset Compensation Configuration
+        0x6C,  // BSCFG         Bit Synchronization Configuration
+        0x03,  // AGCCTRL2      AGC Control
+        0x40,  // AGCCTRL1      AGC Control
+        0x91,  // AGCCTRL0      AGC Control
+        0x87,  // WOREVT1       High Byte Event0 Timeout
+        0x6B,  // WOREVT0       Low Byte Event0 Timeout
+        0xFB,  // WORCTRL       Wake On Radio Control
+        0x56,  // FREND1        Front End RX Configuration
+        0x11,  // FREND0        Front End TX Configuration
+        0xE9,  // FSCAL3        Frequency Synthesizer Calibration
+        0x2A,  // FSCAL2        Frequency Synthesizer Calibration
+        0x00,  // FSCAL1        Frequency Synthesizer Calibration
+        0x1F,  // FSCAL0        Frequency Synthesizer Calibration
+        0x41,  // RCCTRL1       RC Oscillator Configuration
+        0x00,  // RCCTRL0       RC Oscillator Configuration
+        0x59,  // FSTEST        Frequency Synthesizer Calibration Control
+        0x7F,  // PTEST         Production Test
+        0x3F,  // AGCTEST       AGC Test
+        0x81,  // TEST2         Various Test Settings
+        0x35,  // TEST1         Various Test Settings
+        0x09,  // TEST0         Various Test Settings
+};
                //Patable index: -30  -20- -15  -10   0    5    7    10 dBm
 static uint8_t patable_power_315[8] = {0x17,0x1D,0x26,0x69,0x51,0x86,0xCC,0xC3};
 static uint8_t patable_power_433[8] = {0x6C,0x1C,0x06,0x3A,0x51,0x85,0xC8,0xC0};
@@ -611,9 +659,6 @@ void CC1100::wor_reset()
 uint8_t CC1100::tx_payload_burst(uint8_t my_addr, uint8_t rx_addr,
                               uint8_t *txbuffer, uint8_t length)
 {
-    txbuffer[0] = length-1;
-    txbuffer[1] = rx_addr;
-    txbuffer[2] = my_addr;
 
     spi_write_burst(TXFIFO_BURST,txbuffer,length); //writes TX_Buffer +1 because of pktlen must be also transfered
 
@@ -917,6 +962,9 @@ void CC1100::set_mode(uint8_t mode)
                     break;
         case 0x06:
                     spi_write_burst(WRITE_BURST,cc1100_OOK_4_8_kb,CFG_REGISTER);
+                    break;
+        case 0x07:
+                    spi_write_burst(WRITE_BURST,cc1100_OOK_SA_33,CFG_REGISTER);
                     break;
         default:
                     spi_write_burst(WRITE_BURST,cc1100_GFSK_100_kb,CFG_REGISTER);
